aa06e2807e5b2c07c1269d6b48ebc5a26cf8257f
Sam Harwig <42715585+25530@users.noreply.github.com>
2020-05-07 14:58:10 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<form action="post.php" method="post">
+<form action="upload.php" method="post">
  <input type="text" name="text" value="">
  <input type="submit" name="submit" value="Submit">
 </form>
